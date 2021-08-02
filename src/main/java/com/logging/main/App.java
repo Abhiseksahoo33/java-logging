4eddf5b4df5b91f3765693e07a99c4b5416f33bf
Abhisek sahoo <abhiseksahoo33@gmail.com>
2021-08-02 23:38:08 +0530

    Logger file code | Logger code added in logback.xml

--- src/main/java/com/logging/main/App.java
+++ src/main/java/com/logging/main/App.java
@@ -21,14 +21,23 @@ java.lang.ArithmeticException: / by zero
 
 //in real world practice they don't use the terminal stack strace for error they use log file which will contain all the error log data
 
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
+
 public class App {
+    private static final Logger LOGGER= LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
         try{
             int x =10/0;
+        LOGGER.info("result : {} ",x);
+
         }
         catch (ArithmeticException e)
         {
-
+        LOGGER.error("error while executing {}",e.toString(),e);
         }
     }
 }
